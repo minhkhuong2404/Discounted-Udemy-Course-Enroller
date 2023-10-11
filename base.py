8d249f3a2a7b48cf2fa740118ec05c94a21e8906
Lữ Minh Khương <13156@student.vgu.edu.vn>
2023-10-11 19:46:34 +0700

    adjust to make all pages work

--- base.py
+++ base.py
@@ -94,7 +94,7 @@ class Scraper:
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             }
 
-            for page in range(1, 8):
+            for page in range(1, 4):
                 r = requests.get(
                     "https://www.discudemy.com/all/" + str(page), headers=head
                 )
